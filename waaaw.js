5445fd0b5eb20ce622b95c695263184882a231ed
Ahmedtaha13 <150793582+Ahmedtaha13@users.noreply.github.com>
2024-01-22 12:10:56 +0100

    Update waaaw.js

--- waaaw.js
+++ waaaw.js
@@ -1,13 +1,21 @@
-function insertionSort(array) {
-    let i, j, currentElement;
-    for (i = 1; i < array.length; i++) {
-      currentElement = array[i];
-      j = i - 1;
-      while (j >= 0 && array[j] > currentElement) {
-        array[j + 1] = array[j];
-        j--;
-      }
-      array[j + 1] = currentElement;
-    }
-    return array;
-  }
+ALGORITHM simpleSort
+ VAR vector: ARRAY_OF INTEGER=[3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5] i, j: INTEGER
+
+PROCEDURE swap(vector, i, j)
+ VAR temp: INTEGER
+  BEGIN 
+  temp = vector[i]
+   vector[i] = vector[j]
+    vector[j] = temp 
+  END
+
+PROCEDURE simpleSort(vector) VAR n: INTEGER BEGIN n = length(vector)
+
+FOR i FROM 0 TO n - 1 DO
+ FOR j FROM 0 TO n - i - 1 DO 
+ IF vector[j] > vector[j + 1] THEN
+  swap(vector, j, j + 1) 
+  END_IF
+   END_FOR
+    END_FOR
+     END
